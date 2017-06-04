19f2e7a663d42e57f33240d94d9fa41c5a7db159
Djoe Pramono <djoepramono@gmail.com>
2017-06-04 18:32:53 +1000

    add throttling

--- promise/filteredPromise.js
+++ promise/filteredPromise.js
@@ -1,18 +1,34 @@
 'use strict'
 
+
+/**
+ * Promise Without Failing
+ */
+
 const Promise = require('bluebird');
+const Sleep = require('sleep');
+
+const throttleInSeconds = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  let randomValue = (Math.floor(Math.random() * (max - min)) + min);
+  Sleep.sleep(randomValue);
+  return randomValue;
+}
 
 const isBlacklisted = (name) => {
+  console.log('throttling isBlacklisted', throttleInSeconds(1,3));
   return new Promise((pass, fail) => {
     pass(name === 'Trouble');
   })
 };
 
 const getFreebies = (name, membership) => {
+  console.log('throttling getFreebies', throttleInSeconds(1,5));
   return new Promise((pass, fail) => {
       switch(membership) {
-      case "standard": pass([]);
-      case "premium": pass(["free drink"]);
+      case 'standard': pass([]);
+      case 'premium': pass(['free drink']);
       default: fail(`Not a member: ${name}`);
     }
   });
